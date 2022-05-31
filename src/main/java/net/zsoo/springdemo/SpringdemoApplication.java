e94d5d685436a68d711d66d70040ce13b5be4242
Kiho Kim <syspro86@gmail.com>
2022-05-31 03:45:04 +0000

    spring task 샘플코드 추가

--- src/main/java/net/zsoo/springdemo/SpringdemoApplication.java
+++ src/main/java/net/zsoo/springdemo/SpringdemoApplication.java
@@ -1,17 +1,33 @@
 package net.zsoo.springdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
 
 import net.zsoo.springdemo.controller.DemoConfig;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DemoConfig.class)
+@EnableTask
 public class SpringdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdemoApplication.class, args);
 	}
 
+	@Bean
+    public CommandLineRunner commandLineRunner() {
+        return new HelloWorldCommandLineRunner();
+    }
+
+    public static class HelloWorldCommandLineRunner implements CommandLineRunner {
+
+        @Override
+        public void run(String... strings) throws Exception {
+            System.out.println("Hello, World!");
+        }
+    }
 }
